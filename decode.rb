1554e9374c5fe0376a76c4fc0b54a161c43a0406
ybadr99 <yousefbadr909@gmail.com>
2023-07-20 18:02:58 +0300

    fix linters

--- decode.rb
+++ decode.rb
@@ -1,19 +1,16 @@
-$morse_to_char = {
-  '.-'   => 'A',   '-...' => 'B',   '-.-.' => 'C',   '-..'  => 'D',
-  '.'    => 'E',   '..-.' => 'F',   '--.'  => 'G',   '....' => 'H',
-  '..'   => 'I',   '.---' => 'J',   '-.-'  => 'K',   '.-..' => 'L',
-  '--'   => 'M',   '-.'   => 'N',   '---'  => 'O',   '.--.' => 'P',
-  '--.-' => 'Q',   '.-.'  => 'R',   '...'  => 'S',   '-'    => 'T',
-  '..-'  => 'U',   '...-' => 'V',   '.--'  => 'W',   '-..-' => 'X',
-  '-.--' => 'Y',   '--..' => 'Z'
-}
-
-
 def decode_char(char)
-  decoded_char = $morse_to_char[char]
-  if decoded_char.nil?
-    puts "Invalid character encountered: #{char}"
-  end
+  morse_to_char = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-'  => 'K', '.-..' => 'L',
+    '--' => 'M', '-.'   => 'N', '---'  => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
+  }
+
+  decoded_char = morse_to_char[char]
+  puts "Invalid character encountered: #{char}" if decoded_char.nil?
   decoded_char
 end
 
@@ -29,9 +26,9 @@ def decode(message)
   de_message = ''
   message.strip.split('  ').each do |word|
     de_message << decode_word(word)
-    de_message << ' ' unless word == message.split('  ').last  # Add space between words except for the last one
+    de_message << ' ' unless word == message.split('  ').last # Add space between words except for the last one
   end
   print de_message
 end
 
-decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
